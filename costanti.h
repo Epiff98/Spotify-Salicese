01ca5dc1ee22dab24e14cce4cd724ade77720468
Epiff98 <39306345+Epiff98@users.noreply.github.com>
2018-07-13 16:52:02 +0200

    Create costanti.h

---
+++ costanti.h
@@ -0,0 +1,24 @@
+#ifndef COSTANTI_H_
+#define COSTANTI_H_
+
+//-------------------------------------------------------------- DEFINIZIONE COSTANTI SIMBOLICHE -----------------------------------------------------------//
+///Definizione dei valori di alcuni elementi:
+#define LUNG_NOME 30				///LUNGHEZZA MASSIMA DEL CAMPO "NOME": 30 caratteri
+#define LUNG_COGNOME 30				///LUNGHEZZA MASSIMA DEL CAMPO "COGNOME": 30 caratteri
+#define LUNG_USERNAME 15			///LUNGHEZZA MASSIMA DEL CAMPO "USERNAME": 15 caratteri
+#define LUNG_PASSWORD 10			///LUNGHEZZA MASSIMA DEL CAMPO "PASSWORD": 10 caratteri
+#define LUNG_GENERE 30				///LUNGHEZZA MASSIMA DEL CAMPO "GENERE": 30 caratteri
+#define LUNG_CANZONE 30				///LUNGHEZZA MASSIMA DEL CAMPO "CANZONE": 30 caratteri
+#define LUNG_NAZIONALITA 30			///LUNGHEZZA MASSIMA DEL CAMPO "NAZIONALITA": 30 caratteri
+#define LUNG_PREFERENZA 15          ///LUNGHEZZA MASSIMA DEL CAMPO "PREFERENZA": 15 caratteri
+#define N_UTENTI 100				///NUMERO MASSIMO DI UTENTI: 100
+#define N_ARTISTI 100				///NUMERO MASSIMO DI ARTISTI: 100
+#define N_PREFERENZE 100            /// NUMERO MASSIMO PREFERENZE: 100
+#define MESE_MIN 1					///MESE MINIMO: 1 (= GENNAIO)
+#define MESE_MAX 12					///MESE MASSIMO: 12 (= DICEMBRE)
+#define ANNO_MIN 1901				///ANNO MINIMO: 1901
+#define ANNO_MAX 2018				///ANNO MASSIMO: 2018
+#define N_TOP 10					///NUMERO TOP: 10
+//----------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+#endif
