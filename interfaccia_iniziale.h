2371f9c1a29e52bcb96ba45d3352c982ab4bce8b
Epiff98 <39306345+Epiff98@users.noreply.github.com>
2018-07-13 17:00:55 +0200

    Create interfaccia_iniziale.h

---
+++ interfaccia_iniziale.h
@@ -0,0 +1,6 @@
+#ifndef INTERFACCIA_INIZIALE_H_
+#define INTERFACCIA_INIZIALE_H_
+
+void interfaccia_iniziale(FILE* file_artisti, FILE* file_utenti,  FILE* file_preferenze, utenti utente[], artisti artista[], preferenze_artisti preferenza_artista[], int i, char scelta, int z); //PROCEDURA CHE STAMPA A VIDEO IL MENU' INIZIALE DELLA PIATTAFORMA "SPOTIFY"
+
+#endif
